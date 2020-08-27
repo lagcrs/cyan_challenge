02f1e2f9c39ba9eb08ad618c0d2f32fbde7aa289
Larissa Silva <larissa.gcrs@gmail.com>
2020-08-27 13:08:11 -0300

    Cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 const routes = require('./routes');
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
