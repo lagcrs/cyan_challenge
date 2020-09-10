8dceb5376f95e5bbe61d40cc624d0dc409ef24f9
Larissa Silva <larissa.gcrs@gmail.com>
2020-09-09 21:47:04 -0300

    add filter route in react

--- web/src/pages/Filter/index.js
+++ web/src/pages/Filter/index.js
@@ -6,43 +6,147 @@ import Button from '../../components/Button';
 import FormSelect from '../../components/FormSelect';
 import './styles.css';
 
+
+const FilterHarvest = (props) => {
+	const [show, setShow] = useState(false);
+	const [farms, setFarms] = useState({});
+	const [harvestCode, setHarvestCode] = useState('');
+	const options = props.data;
+
+	var farmCodeData = [];
+	for (var i in farms) {
+	 	farmCodeData.push(farms[i]['code'])
+	}
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		setShow(true);
+
+		const response = await api.get('/filter-harvest', {
+			params: {
+				harvest_code: harvestCode
+			},
+		});
+		setFarms(response.data);
+	}
+
+	return (
+		<>
+			<div id="content-filter-2">
+				<form>
+					<FormSelect label="Escolha um código de safra abaixo para fazer a filtragem." onChange={e =>{ setHarvestCode(e.target.value); } } options={options}/>
+					<Button onClick={handleSubmit}>Filtrar Safra</Button>
+				</form>
+			</div>
+			{ show ? <FilterFarm  data={farmCodeData}/>	: null}
+		</>
+	)
+}
+
+const FilterFarm = (props) => {
+	const [show, setShow] = useState(false);
+	const [farmCode, setFarmCode] = useState('');
+	const [fields, setFields] = useState({});
+
+	const options = props.data;
+
+	var fieldCodeData = [];
+	 for (var i in fields) {
+	 	fieldCodeData.push(fields[i]['code'])
+	}
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		setShow(true);
+		const response = await api.get('/filter-farm', {
+			params: {
+				farm_code: farmCode
+			},
+		});
+		setFields(response.data);
+	}
+	console.log(fields)
+
+	return (
+		<>
+			<div id="content-filter-2">
+				<form>
+					<FormSelect label="Escolha uma fazenda abaixo para fazer a filtragem." onChange={e =>{ setFarmCode(e.target.value) } } options={options}/>
+					<Button onClick={handleSubmit}>Filtrar Fazenda</Button>
+				</form>
+			</div>
+			{ show ? <FilterField  data={fieldCodeData}/>	: null}
+		</>
+	)
+}
+
+const FilterField = (props) => {
+	const [fieldCode, setFieldCode] = useState('');
+	const [fieldsResults, setFieldsResults] = useState('');
+
+	const options = props.data;
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		const response = await api.get('/filter-field', {
+			params: {
+				field_code: fieldCode
+			},
+		});
+		setFieldsResults(response.data);
+	}
+	console.log(fieldsResults)
+
+	return (
+		<>
+			<div id="content-filter-2">
+				<form>
+					<FormSelect label="Escolha um código de campo abaixo para fazer a filtragem." onChange={e =>{ setFieldCode(e.target.value) } } options={options}/>
+					<Button onClick={handleSubmit}>Filtrar Campo</Button>
+				</form>
+			</div>
+		</>
+	)
+}
+
 function Filter() {
 	const [show, setShow] = useState(false);
 	
 	const [mill, setMill] = useState("");
 	const [harvestStart, setHarvestStart] = useState('');
 	const [harvestEnd, setHarvestEnd] = useState('');
-	const [harvestCode, setHarvestCode] = useState('');
-	const [farmName, setFarmName] = useState('');
-	const [farmCode, setFarmCode] = useState('');
-	const [fieldCode, setFieldCode] = useState('');
+	
+	
+	
 
 	const [mills, setMills] = useState("");
 	const [harvests, setHarvests] = useState({});
-	const [farms, setFarms] = useState({});
-	const [fields, setFields] = useState({});
+	
+	
 
 	useEffect(() => {
 		async function load(){
 			const response_mill = await api.get('/mills');
 			setMills(response_mill.data);
-
-			const response_harvest = await api.get('/harvests');
-			setHarvests(response_harvest.data);
-
-			const response_farm = await api.get('/farms');
-			setFarms(response_farm.data);
-
-			const response_field = await api.get('/fields');
-			setFields(response_field.data);
 		};
 		load();
 	}, []);
+
+	
+
+	
 	
 	var millName = [];
 	for (var i in mills) {
 		millName.push(mills[i]['name'])
 	}
+	
+	var harvestCodeData = [];
+	for (var i in harvests) {
+		harvestCodeData.push(harvests[i]['code'])
+	}
+
+
 
 	var harvestStartData = [];
 	for (var i in harvests) {
@@ -54,57 +158,46 @@ function Filter() {
 		harvestEndData.push(harvests[i]['end'])
 	}
 	
-	var harvestCodeData = [];
-	for (var i in harvests) {
-		harvestCodeData.push(harvests[i]['code'])
-	}
 	
-	var farmNameData = [];
-	for (var i in farms) {
-		farmNameData.push(farms[i]['name'])
-	}
+	// var farmNameData = [];
+	// for (var i in farms) {
+	// 	farmNameData.push(farms[i]['name'])
+	// }
 	
-	var farmCodeData = [];
-	for (var i in farms) {
-		farmCodeData.push(farms[i]['code'])
-	}
+	// var farmCodeData = [];
+	// for (var i in farms) {
+	// 	farmCodeData.push(farms[i]['code'])
+	// }
 	
-	var fieldCodeData = [];
-	for (var i in fields) {
-		fieldCodeData.push(fields[i]['code'])
-	}
-	
-	async function handleSearch(e){
+	// var fieldCodeData = [];
+	// for (var i in fields) {
+	// 	fieldCodeData.push(fields[i]['code'])
+	//}
+
+	const handleSubmit = async e => {
 		e.preventDefault();
-		const response = await api.get('/filter', {
+		setShow(true)
+		const response = await api.get('/filter-mill', {
 			params: {
-				mill_name: mill,
-				harvest_start: harvestStart,
-				harvest_end: harvestEnd,
-				harvest_code: harvestCode,
-				farm_name: farmName,
-				farm_code: farmCode,
-				field_code: fieldCode
+				mill_name: mill
 			},
-		  });
-	  
-		  console.log(response.data);
+		});
+		setHarvests(response.data);
 	}
-	
+
 	return (
 		<>
-		<Menu to="/" text="Home" />
-		<header className="header-filter">
-				<form>
-					<FormSelect label="Escolha uma usina abaixo para fazer a filtragem." onChange={e =>{ setMill(e.target.value) }} options={millName}/>
-					<FormSelect label="Escolha data de início da Safra." onChange={e =>{ setHarvestStart(e.target.value) }} options={harvestStartData}/>
-					<FormSelect label="Escolha data final da safra." onChange={e =>{ setHarvestEnd(e.target.value) }} options={harvestEndData}/>
-					<FormSelect label="Escolha código da safra." onChange={e =>{ setHarvestCode(e.target.value) }} options={harvestCodeData}/>
-					<FormSelect label="Escolha nome da fazenda." onChange={e =>{ setFarmName(e.target.value) }} options={farmNameData}/>
-					<FormSelect label="Escolha código da fazenda." onChange={e =>{ setFarmCode(e.target.value) }} options={farmCodeData}/>
-					<FormSelect label="Escolha código do talhão." onChange={e =>{ setFieldCode(e.target.value) }} options={fieldCodeData}/>
-					<Button onClick={handleSearch}>Realizar Filtragem</Button>
-				</form>
+			<Menu to="/" text="Home" />
+			<header className="header-filter">
+				<div className="container">
+					<div id="content-filter">
+						<form >
+							<FormSelect label="Escolha uma usina abaixo para fazer a filtragem." onChange={e =>{ setMill(e.target.value); } } options={millName}/>
+							<Button onClick={handleSubmit} className="click">Filtrar Usina</Button>
+						</form>
+						{ show ? <FilterHarvest  data={harvestCodeData}/>	: null}
+					</div>
+				</div>
 			</header>
 		</>);
 	}
